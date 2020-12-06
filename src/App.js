947169824bce73c18c7b836c7176b326d1275f85
mykolao <mykolaoleskiv@gmail.com>
2020-12-06 08:22:48 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const API_BASE = "https://economic-little-dog.glitch.me/";
+const getValue = API_BASE;
+const increaseValue = API_BASE + "increase";
+const decreaseValue = API_BASE + "decrease";
+const mget = { method: "GET" };
+const mpost = { method: "POST" };
+
+let errFlag = false;
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const update = (url, method) => {
+    if (!errFlag) {
+      fetch(url, method)
+        .then((res) => res.json())
+        .then((data) => {
+          setCount(data.value);
+        })
+        .catch((err) => {
+          console.error(err);
+          errFlag = true;
+        });
+    }
+  };
+
+  const increaseCounter = () => {
+    update(increaseValue, mpost);
+  };
+
+  const decreaseCounter = () => {
+    update(decreaseValue, mpost);
+  };
+
+  const getCounter = () => {
+    update(getValue, mget);
+  }
+
+  getCounter()
+
+  setInterval(() => {
+    getCounter();
+  }, 3500);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="container">
+      <header className="header">
+        <h1>The Useless Counter</h1>
       </header>
-    </div>
+      <p className="counter">{count}</p>
+      <div className="group-btn">
+        <button onClick={decreaseCounter} className="btn decrease-btn">
+          Decrease
+        </button>
+        <button onClick={increaseCounter} className="btn increase-btn">
+          Increase
+        </button>
+      </div>
+    </main>
   );
 }
 
